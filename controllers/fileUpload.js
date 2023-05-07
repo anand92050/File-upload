9b7377e1989d13f0dcdff922a55073bb0179e5f1
Anand Raj <89471340+anand92050@users.noreply.github.com>
2023-05-07 21:47:25 +0530

    Update fileUpload.js

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -13,12 +13,16 @@ exports.LocalFileUpload = async (req,res) =>{
             console.log(err);
         })
 
-        res.json({
+        return res.status(202).json({
             success:true,
             message:"local file upload successfully",
-        })
+        });
     }
     catch(err){
         console.log(err);
+        return res.status(500).json({
+            success:false,
+            message:'file not upload',
+        });
     }
 }
